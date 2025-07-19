4b25612dd920ced0c776f91873cf6ed44237f859
Muhammed Aflah <aflahtbm@gmail.com>
2025-07-19 07:31:35 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -71,9 +71,7 @@ async def save_group(bot, message):
                     reply_markup=InlineKeyboardMarkup(button),
                     parse_mode=enums.ParseMode.HTML
                 )  
-        if settings[""]:
-            await asyncio.sleep()
-            await (temp.MELCOW['welcome']).()
+        
 
 @Client.on_message(filters.command('leave') & filters.user(ADMINS))
 async def leave_a_chat(bot, message):

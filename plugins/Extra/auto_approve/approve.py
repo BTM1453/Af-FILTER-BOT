bcb54d19974d1155596989e1c1b1578047579593
Muhammed Aflah <aflahtbm@gmail.com>
2025-08-03 09:57:04 +0530

    Update approve.py

--- plugins/Extra/auto_approve/approve.py
+++ plugins/Extra/auto_approve/approve.py
@@ -93,11 +93,12 @@ async def auto_approve(client, message: ChatJoinRequest):
             reply_markup = InlineKeyboardMarkup(buttons)
             m=await message.reply_sticker("CAACAgUAAxkBAAEKVaxlCWGs1Ri6ti45xliLiUeweCnu4AACBAADwSQxMYnlHW4Ls8gQMAQ") 
             
-                           await asyncio.sleep(1)
+            await asyncio.sleep(1)
             await m.delete()
             await message.reply_photo(
                 photo=random.choice(PICS),
-                caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup,
+                caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME), 
+           reply_markup=reply_markup,
                 parse_mode=enums.ParseMode.HTML
             )
             return 

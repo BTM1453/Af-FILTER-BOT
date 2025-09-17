2930655ade57c3d04be473ce5bb4efb9090a4f5e
Muhammed Aflah <aflahtbm@gmail.com>
2025-09-17 19:29:27 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -238,12 +238,11 @@ Nᴀᴍᴇ - {}"""
 
     CAPTION = """<b>{file_name}
 
-☞ <a href="https://t.me/+FHe4nVjjxrI1MDc9">𝙾ᴛᴛ ᴜᴩᴅᴀᴛᴇ 𝙲ʜᴀɴɴᴇʟ</a> 
-☞ <a href="https://t.me/CINEMA_COMPANYE_Backup"> 𝙼ᴏᴠɪᴇ 𝚂ᴇᴀʀᴄʜ 𝙶ʀᴏᴜᴩ - 𝟷</a> 
-☞ <a href="https://t.me/+MOrvSbM3zfVjNmRl"> 𝙼ᴏᴠɪᴇ 𝚂ᴇᴀʀᴄʜ 𝙶ʀᴏᴜᴩ - 𝟸</a> 
-☞ <a href="https://t.me/TBM_Bot_Updates"> 𝙱ᴏᴛ 𝚄ᴩᴅᴀᴛᴇꜱ 𝙲ʜᴀɴɴᴇʟ</a>
-
-🔰 𝚂𝚑𝚊𝚛𝚎 🔰 𝚂𝚞𝚙𝚙𝚘𝚛𝚝 🔰</b>"""
+╔════ ᴊᴏɪɴ ᴡɪᴛʜ ᴜs ════╗ 
+➲ <a href="https://t.me/CINEMA_COMPANYE_Backup">𝐂𝐈𝐍𝐄𝐌𝐀 𝐂𝐎𝐌𝐏𝐀𝐍𝐘</a><br>
+➲ <a href="https://t.me/+JObo3SXOiZRlNzll">𝐅𝐑𝐈𝐃𝐀𝐘 𝐅𝐈𝐋𝐈𝐌 𝐇𝐎𝐔𝐒𝐄</a><br>
+➲ <a href="https://t.me/+fgT9ogX7z144NjM1">𝐋𝐀𝐓𝐄𝐒𝐓 𝐁𝐎𝐓 𝐔𝐏𝐃𝐀𝐓𝐄</a><br>
+╚════ ᴊᴏɪɴ ᴡɪᴛʜ ᴜs ════╝</b>"""
 
     IMDB_TEMPLATE_TXT = """
 <b>Query: {qurey}
